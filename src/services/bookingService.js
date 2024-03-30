e7a3b0c63032be18a9f6f37b0ca03737eee0deaa
HieuNguyen <nguyenhieu11032002@gmail.com>
2024-03-30 13:28:00 +0700

    fix: update body in feature change password

--- src/services/bookingService.js
+++ src/services/bookingService.js
@@ -8,9 +8,9 @@ export const createBooking = (userId, body) =>
         attributes: ["estimated_duration"],
       });
       // Chuyển đổi thành số nguyên
-      const [hours, minutes, seconds] = body.start_time.split(":").map(Number);
+      let [hours, minutes, seconds] = body.start_time.split(":").map(Number);
       // Cộng thêm 30 phút
-      const newMinutes = minutes + duration.dataValues.estimated_duration;
+      let newMinutes = minutes + duration.dataValues.estimated_duration;
 
       // Xử lý nếu phút vượt quá 60
       if (newMinutes >= 60) {
@@ -22,11 +22,11 @@ export const createBooking = (userId, body) =>
       const newTimeString = `${hours.toString().padStart(2, "0")}:${newMinutes
         .toString()
         .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-      console.log(newTimeString);
-      //   const result = await db.Booking.create({
-      //     ...body,
-      //     user_id: userId,
-      //   });
+      const result = await db.Booking.create({
+        ...body,
+        user_id: userId,
+        end_time: newTimeString,
+      });
       resolve({
         success: duration ? true : false,
         message: duration ? "Create pet successfully" : "Create pet failed",
