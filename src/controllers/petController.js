3ef915c76445d5f4065601b5d6a722db460b8a67
HieuNguyen <nguyenhieu11032002@gmail.com>
2024-04-07 15:45:10 +0700

    fix: delete validate image

--- src/controllers/petController.js
+++ src/controllers/petController.js
@@ -4,8 +4,6 @@ import joi from "joi";
 export const createPet = async (req, res) => {
   try {
     const fileData = req.file;
-    if (!fileData)
-      return res.status(400).json({ message: "Image is required" });
     const { error } = joi.object().validate({ image: fileData?.path });
     if (error) {
       if (fileData) cloudinary.uploader.destroy(fileData.filename);
@@ -21,8 +19,6 @@ export const createPet = async (req, res) => {
 export const updatePet = async (req, res) => {
   try {
     const fileData = req.file;
-    if (!fileData)
-      return res.status(400).json({ message: "Image is required" });
     const { error } = joi.object().validate({ image: fileData?.path });
     if (error) {
       if (fileData) cloudinary.uploader.destroy(fileData.filename);
